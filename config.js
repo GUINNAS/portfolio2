e0cb1a5247cc88fc61f4f4a75905966aa40f3cc7
GUINNAS <guimassucato017@gmail.com>
2025-03-17 18:31:38 -0300

    portfolio v1.0

--- config.js
+++ config.js
@@ -13,3 +13,37 @@ tabs.forEach((tab, index) => {
     contents[index].classList.add('active');
   });
 });
+
+const navbar = document.querySelector('.navbar');
+const links = navbar.querySelectorAll('a');
+
+links.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+const botoes = document.querySelectorAll('.botao-especialidade');
+
+botoes.forEach((botao) => {
+  botao.addEventListener('click', () => {
+    const target = botao.getAttribute('data-target');
+    const div = document.querySelector(target);
+    if (div) {
+      const divs = document.querySelectorAll('.conteudo-especialidades div');
+      divs.forEach((d) => {
+        d.style.display = 'none';
+      });
+      div.style.display = 'block';
+      botoes.forEach((b) => {
+        b.classList.remove('ativo');
+      });
+      botao.classList.add('ativo');
+    } else {
+      console.error(`Div não encontrada: ${target}`);
+    }
+  });
+});
